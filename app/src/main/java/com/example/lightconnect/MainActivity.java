29b874bc82b77dcdfd2961a4a2ec71257c39b9b7
Sam Seaberry <seaberrysam@gmail.com>
2022-02-15 12:17:09 -0800

    BLE Connection and READ/WRITE

--- app/src/main/java/com/example/lightconnect/MainActivity.java
+++ app/src/main/java/com/example/lightconnect/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.lightconnect;
 import android.Manifest;
 import android.annotation.SuppressLint;
 
+import android.app.Application;
 import android.app.Dialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -15,11 +17,13 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 
+import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.appcompat.app.AppCompatActivity;
@@ -76,44 +80,32 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     private ArrayList<presets_items> mPresetList;
     private PresetsAdapter mAdapter;
 
-
+    private RGBvalues mRGB;
     //new instances
-    private ImageButton mBLEButton;
     private Button mSaveButton;
-
     private Button mApplyButton;
 
     //sql instances
     private SQL_base mSQL;
     private FrameLayout mFrame;
 
-    //for ble spinner
-    private AdapterData mBLEData;
-    private Spinner mSpinnerList;
-    private BleAdapter mBLEAdapter;
-
-    //BLE and other instances
-    private BluetoothAdapter mBTAdapter;
-    private Set<BluetoothDevice> mPairedDevices;
-    private ArrayAdapter<String> mBTArrayAdapter;
+    private Handler mHandler;
 
 
-    private Handler mHandler; // Our main handler that will receive callback notifications
-    private ConnectedThread mConnectedThread; // bluetooth background worker thread to send and receive data
-    private BluetoothSocket mBTSocket=null; // bi-directional client-to-client data path
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRGB = new RGBvalues();
 
+        red = mRGB.getRED();
+        blue = mRGB.getBLUE();
+        green = mRGB.getGREEN();
 
-
-        //bluetooth connection checking
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(BluetoothDevice.ACTION_ACL_CONNECTED);
-        filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED);
+        Bluetooth_Fragment fragment = (Bluetooth_Fragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
 
         //colorwheel
         mColourWheel = findViewById(R.id.imageView);
@@ -129,10 +121,10 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
 
         mFrame = findViewById(R.id.framelay);
 
-        mSpinnerList = findViewById(R.id.spinner1);
+        //mSpinnerList = findViewById(R.id.spinner1);
         mSQL = new SQL_base(MainActivity.this);
 
-        mBLEButton = findViewById(R.id.simpleImageButton);
+
         mSaveButton = findViewById(R.id.button4);
         mApplyButton = findViewById(R.id.button3);
 
@@ -173,80 +165,18 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         });
 
 
-        mBLEData = new AdapterData();
-        mBLEData.loadAdapterData();
-        //bluetooth spinner and setup
-
-        mBTArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item);
-        mBTAdapter = BluetoothAdapter.getDefaultAdapter(); // get a handle on the bluetooth radio
-        listPairedDevices();
-        mBTArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-
-
-        mSpinnerList.setAdapter(mBTArrayAdapter); // assign model to view
-
-
-
-        // Ask for location permission if not already allowed
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
-
-        //what happens on device selection from drop down
-        mSpinnerList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
-                if(!mBTAdapter.isEnabled()) {
-                    Toast.makeText(getBaseContext(), "Bluetooth not on", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                String info = ((TextView) view).getText().toString();//reading device name from sql spinner
-                if(info.isEmpty()){
-                    Toast.makeText(getBaseContext(), "Bluetooth Device Not Selected", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                final String address = getBLEaddress(info);
-
-                mConnectedThread = new ConnectedThread(mBTSocket,mHandler,mBTAdapter,address);
-
-
-                mConnectedThread.BTconnect(); //connect selected device
-
-
-                if(mConnectedThread.isConnected()){
-                    Toast.makeText(getBaseContext(), "Bluetooth Device Connected", Toast.LENGTH_SHORT).show();
-                    mSpinnerList.setBackgroundResource(R.drawable.style_green_ble_icon);
-                }else{
-                    Toast.makeText(getBaseContext(), "Bluetooth Device Not Connected", Toast.LENGTH_SHORT).show();
-
-                }
-
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-                listPairedDevices();
-            }
-        });
-
-
         //colourwheel listener so users can select RGB colour option from image
         mColourWheel.setOnTouchListener((v, event) -> {
             if(event.getAction()== MotionEvent.ACTION_DOWN || event.getAction()== MotionEvent.ACTION_MOVE){
                 bitmap = mColourWheel.getDrawingCache();
                 int pixels = bitmap.getPixel((int)event.getX(), (int)event.getY());
 
-                red = Color.red(pixels);
-                green = Color.green(pixels);
-                blue = Color.blue(pixels);
+                mRGB.setALL(Color.red(pixels), Color.green(pixels), Color.blue(pixels));
 
                 setBarValue();
 
-                mColorView.setBackgroundColor(Color.rgb(red,green,blue));
-                mFrame.setElevationShadowColor(Color.rgb(red,green,blue));
+                mColorView.setBackgroundColor(Color.rgb(mRGB.getRED(),mRGB.getGREEN(),mRGB.getBLUE()));
+                mFrame.setElevationShadowColor(Color.rgb(mRGB.getRED(),mRGB.getGREEN(),mRGB.getBLUE()));
             }
             return true;
         });
@@ -278,36 +208,22 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
             }
         };
 
-        if (mBTArrayAdapter == null) {
-            // Device does not support Bluetooth
-            //mBluetoothStatus.setText("Status: Bluetooth not found");
-            Toast.makeText(getApplicationContext(),"Bluetooth device not found!",Toast.LENGTH_SHORT).show();
-        }
-        else {
-
-            mApplyButton.setOnClickListener(v -> {
-                String send = "1-"+red+"-2-"+green+"-3-"+blue+"-4-"+brightness; //this string is set so that when sent to a microcontroller running "Light Connect Micro" it can set each color value to the respecting pin
-                mConnectedThread.writeString(send);
-            });
 
 
-            mBLEButton.setOnClickListener(v->{
-                if(!mBTAdapter.isEnabled()){
-                    bluetoothOn();
-                    mBLEButton.setBackgroundResource(R.drawable.style_ble_spinner1);
-                }else{
-                    bluetoothOff();
-                    mBLEButton.setBackgroundResource(R.drawable.style_ble_spinner_red);
+        mApplyButton.setOnClickListener(v -> {
+            assert fragment != null;
+            fragment.writecharacteristic();
+        });
 
-                }
-            });
+
+
 
             //mSaveButton.setOnClickListener(v -> bluetoothOff());
 
-            mSaveButton.setOnClickListener(v -> newpreset());
+        mSaveButton.setOnClickListener(v -> newpreset());
 
 
-        }
+
 
     }
 
@@ -315,80 +231,10 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
 
     private void setRGB(){
         //setting the colour preview
-        mColorView.setBackgroundColor(Color.rgb(red,green,blue));
-        mFrame.setElevationShadowColor(Color.rgb(red,green,blue));
+        mColorView.setBackgroundColor(Color.rgb(mRGB.getRED(),mRGB.getGREEN(),mRGB.getBLUE()));
+        mFrame.setElevationShadowColor(Color.rgb(mRGB.getRED(),mRGB.getGREEN(),mRGB.getBLUE()));
 
 
-    }
-
-    private void bluetoothOn(){
-        if (!mBTAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-            Toast.makeText(getApplicationContext(),"Bluetooth turned on",Toast.LENGTH_SHORT).show();
-
-        }
-        else{
-            Toast.makeText(getApplicationContext(),"Bluetooth is already on", Toast.LENGTH_SHORT).show();
-
-        }
-    }
-    /*
-    // Enter here after user selects "yes" or "no" to enabling radio
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent Data) {
-        // Check which request we're responding to
-        super.onActivityResult(requestCode, resultCode, Data);
-        if (requestCode == REQUEST_ENABLE_BT) {
-            // Make sure the request was successful
-            if (resultCode == RESULT_OK) {
-                // The user picked a contact.
-                // The Intent's data Uri identifies which contact was selected.
-                Toast.makeText(getApplicationContext(),"Bluetooth turned ON", Toast.LENGTH_SHORT).show();
-                updateDeviceList();
-            } else
-                Toast.makeText(getApplicationContext(),"Bluetooth turned Off", Toast.LENGTH_SHORT).show();
-        }
-    }*/
-
-    private void bluetoothOff(){
-        mBTAdapter.disable(); // turn off
-        //mBluetoothStatus.setText("Bluetooth disabled");
-        Toast.makeText(getApplicationContext(),"Bluetooth turned Off", Toast.LENGTH_SHORT).show();
-    }
-    private void updateDeviceList(){
-        listPairedDevices();
-        mSpinnerList.setAdapter(mBTArrayAdapter);
-    }
-
-
-    private void listPairedDevices(){
-        mBTArrayAdapter.clear();
-        mPairedDevices = mBTAdapter.getBondedDevices();
-        if(mBTAdapter.isEnabled()) {
-            // put it's one to the adapter
-            for (BluetoothDevice device : mPairedDevices)
-                //mBTArrayAdapter.add(device.getName() + "\n" + device.getAddress());
-                mBTArrayAdapter.add(device.getName());
-            Toast.makeText(getApplicationContext(), "Show Paired Devices", Toast.LENGTH_SHORT).show();
-        }
-        else
-            Toast.makeText(getApplicationContext(), "Bluetooth not on", Toast.LENGTH_SHORT).show();
-
-    }
-    private String getBLEaddress(String name){
-
-        mPairedDevices = mBTAdapter.getBondedDevices();
-        if(mBTAdapter.isEnabled()) {
-            // put it's one to the adapter
-            for (BluetoothDevice device : mPairedDevices)
-                if(Objects.equals(device.getName(),name)){
-                    return device.getAddress();
-                }
-
-
-        }
-        return "fail";
     }
 
 
@@ -396,13 +242,13 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
         switch(seekBar.getId()){
             case R.id.seekBar:
-                red=progress;
+                mRGB.setRED(progress);
                 break;
             case R.id.seekBar2:
-                green = progress;
+                mRGB.setGREEN(progress);
                 break;
             case R.id.seekBar3:
-                blue = progress;
+                mRGB.setBLUE(progress);
                 break;
             case R.id.seekBar4:
                 brightness = progress;
@@ -410,8 +256,8 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
 
 
         }
-        mColorView.setBackgroundColor(Color.rgb(red,green,blue));
-        mFrame.setElevationShadowColor(Color.rgb(red,green,blue));
+        mColorView.setBackgroundColor(Color.rgb(mRGB.getRED(),mRGB.getGREEN(),mRGB.getBLUE()));
+        mFrame.setElevationShadowColor(Color.rgb(mRGB.getRED(),mRGB.getGREEN(),mRGB.getBLUE()));
     }
 
     @Override
@@ -445,9 +291,9 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         Log.v("EditText", mInputName.getText().toString());
         savebtn.setOnClickListener(v -> {
             String presetname1 = mInputName.getText().toString();
-            addpresettolist(presetname1,red, green, blue);
+            addpresettolist(presetname1,mRGB.getRED(),mRGB.getGREEN(),mRGB.getBLUE());
 
-            mSQL.addNewPreset(presetname1,red, green, blue,brightness);
+            mSQL.addNewPreset(presetname1,mRGB.getRED(),mRGB.getGREEN(),mRGB.getBLUE(),brightness);
 
             dialog.dismiss();
         });
@@ -455,14 +301,15 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         ImageView btnclose = dialog.findViewById(R.id.close_btn);
         btnclose.setOnClickListener(v -> dialog.dismiss());
         dialog.show();
+        loadSpinnerData();
 
     }
 
     //sets the seek bar values
     private void setBarValue(){
-        mRed.setProgress(red);
-        mGreen.setProgress(green);
-        mBlue.setProgress(blue);
+        mRed.setProgress(mRGB.getRED());
+        mGreen.setProgress(mRGB.getGREEN());
+        mBlue.setProgress(mRGB.getBLUE());
         mBrightness.setProgress(brightness);
     }
 
@@ -475,10 +322,10 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         String queryString = "SELECT * FROM " + "presets" + " WHERE " + "preset" + " = ?";
         Cursor cursor = mSQL.getData(selected);
         cursor.moveToFirst();
-        if(cursor!=null && cursor.getCount()>0){
-            red = cursor.getInt(1);
-            green = cursor.getInt(2);
-            blue = cursor.getInt(3);
+        if(cursor.getCount()>0){
+            mRGB.setRED(cursor.getInt(1));
+            mRGB.setGREEN(cursor.getInt(2));
+            mRGB.setBLUE(cursor.getInt(3));
             brightness = cursor.getInt(4);
         }
         setRGB();
@@ -503,22 +350,6 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         mPresets.setAdapter(dataAdapter);
     }
 
-
-
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-
-            if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action))  {
-                connected = true;
-            }
-            else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
-                connected = false;
-            }
-        }
-    };
 
 
 
